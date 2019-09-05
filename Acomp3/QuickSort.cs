079a923549d2dd70414532aadfb3cd18b1af7905
Sávio Da Silva Lopes <20171ENEL0322@cefetes.br>
2019-09-05 09:59:08 -0300

    terminado e enviado 05/09

--- Acomp3/QuickSort.cs
+++ Acomp3/QuickSort.cs
@@ -6,17 +6,37 @@ namespace Acomp3
     {
         public int Part(int[] E, int left, int right)
         {
-            throw new NotImplementedException();
+            int c = E[right]; 
+            int t, j = left;
+            for (int k = left; k < right; k++)
+            {
+                if (E[k] <= c)
+                {
+                    t = E[j];
+                    E[j] = E[k];
+                    E[k] = t;
+                    j++;
+                }
+            }
+            t = E[j];
+            E[j] = E[right];
+            E[right] = t;
+            return j;
         }
 
         public void Sort(int[] E, int left, int right)
         {
-            throw new NotImplementedException();
+            if (left < right)
+            {
+                int b = Part(E, left, right);
+                Sort(E, left, b - 1);
+                Sort(E, b + 1, right);
+            }
         }
 
         public void Sort(int[] E, int n)
         {
-            Sort(E, 0, n-1);
+            Sort(E, 0, n - 1);
         }
     }
 }
--- Acomp3/TailQuickSort.cs
+++ Acomp3/TailQuickSort.cs
@@ -1,12 +1,49 @@
 ﻿using System;
-
 namespace Acomp3
 {
-    class TailQuickSort : QuickSort
+    class TailQuickSort : IQuickSort
     {
-        public new void Sort(int[] E, int left, int right)
+        public int Part(int[] E, int left, int right)
         {
-            throw new NotImplementedException();
+            int c = E[right];
+            int t, j = left;
+            for (int k = left; k < right; k++)
+            {
+                if (E[k] <= c)
+                {
+                    t = E[j];
+                    E[j] = E[k];
+                    E[k] = t;
+                    j++;
+                }
+            }
+            t = E[j];
+            E[j] = E[right];
+            E[right] = t;
+            return j;
+        }
+
+        public void Sort(int[] E, int left, int right)
+        {
+            while (left < right)
+            {
+                int j = Part(E, left, right);
+                if (j - left < right - j)
+                {
+                    Sort(E, left, j - 1);
+                    left = j + 1;
+                }
+                else
+                {
+                    Sort(E, j + 1, right);
+                    right = j - 1;
+                }
+            }
+        }
+
+        public void Sort(int[] E, int n)
+        {
+            Sort(E, 0, n - 1);
         }
     }
 }
